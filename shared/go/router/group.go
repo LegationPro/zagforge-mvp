b9447c6ea134ee3ad615a36d379fb8796f62fd46
legationpro <peternelanze@gmail.com>
2026-03-19 10:56:35 +0100

    chore: create abstraction

---
+++ shared/go/router/group.go
@@ -0,0 +1,84 @@
+package router
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+// Compile-time check
+var _ Grouper = (*Group)(nil)
+
+type Method string
+
+const (
+	GET    Method = "GET"
+	POST   Method = "POST"
+	PUT    Method = "PUT"
+	DELETE Method = "DELETE"
+	PATCH  Method = "PATCH"
+)
+
+type Subroute struct {
+	Method  Method
+	Path    string
+	Handler http.HandlerFunc
+}
+
+type Group struct {
+	mux *chi.Mux
+	mw  func(http.Handler) http.Handler
+}
+
+func NewGroup(mux *chi.Mux) *Group {
+	return &Group{mux: mux}
+}
+
+// Use sets the middleware applied to all subroutes in this group.
+func (g *Group) Use(mw func(http.Handler) http.Handler) {
+	g.mw = mw
+}
+
+// Create registers all subroutes on the group's mux.
+// If middleware is set via Use, routes are wrapped in a chi group with that middleware.
+func (g *Group) Create(subroutes []Subroute) error {
+	var errs []error
+
+	methods := map[Method]func(chi.Router, string, http.HandlerFunc){
+		GET:    func(r chi.Router, p string, h http.HandlerFunc) { r.Get(p, h) },
+		POST:   func(r chi.Router, p string, h http.HandlerFunc) { r.Post(p, h) },
+		PUT:    func(r chi.Router, p string, h http.HandlerFunc) { r.Put(p, h) },
+		DELETE: func(r chi.Router, p string, h http.HandlerFunc) { r.Delete(p, h) },
+		PATCH:  func(r chi.Router, p string, h http.HandlerFunc) { r.Patch(p, h) },
+	}
+
+	register := func(r chi.Router) {
+		for _, sub := range subroutes {
+			if fn, ok := methods[sub.Method]; ok {
+				fn(r, sub.Path, sub.Handler)
+			} else {
+				errs = append(errs,
+					fmt.Errorf("failed to register path: %q with method: %q", sub.Path, sub.Method),
+				)
+			}
+		}
+	}
+
+	if g.mw != nil {
+		g.mux.Group(func(r chi.Router) {
+			r.Use(g.mw)
+			register(r)
+		})
+	} else {
+		register(g.mux)
+	}
+
+	return errors.Join(errs...)
+}
+
+// Grouper is the interface satisfied by Group.
+type Grouper interface {
+	Create(subroutes []Subroute) error
+}
